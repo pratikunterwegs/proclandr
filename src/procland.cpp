10a4635be062b783bb8059608472c1e96857c96f
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-02 16:00:52 +0200

    land now outputs

--- src/procland.cpp
+++ src/procland.cpp
@@ -8,9 +8,24 @@ using namespace Rcpp;
 using namespace noise;
 
 // a function that makes wrapped perlin noise
+//' Prints Perlin noise to file.
+//' 
+//' @param nOctaves Number of octaves. May be thought of as small scale 
+//' variability. Must be an integer value between 1 and 8. Higher values
+//' result in landscapes with more small scale noise.
+//' @param frequency Frequency of noise. May be thought of as large scale
+//' variability. May be any double value between 1.0 and 16.0. Higher values
+//' result in noise that is closer to white noise.
+//' @param landSize Landscape size in pixels. The image saved will be a
+//' square with this side.
+//' @param isWrapped A boolean value indicating whether the landscape is
+//' tiled, that is, whether the pattern repeats. Useful when generating
+//' landscapes for simulations.
+//' @param outputPath A string specifying the image file to write the landscape.
+//' @return Nothing. Writes an image to file.
 // [[Rcpp::export]]
-void getPerlin(const int nOctaves, const float frequency,
-               const float landSize, const bool isWrapped,
+void getPerlin(const int nOctaves, const double frequency,
+               const double landSize, const bool isWrapped,
                std::string outputPath) {
     
     // make Perlin noise
@@ -25,7 +40,7 @@ void getPerlin(const int nOctaves, const float frequency,
     mapBuilder.SetSourceModule(noise);
     mapBuilder.SetDestNoiseMap(thisMap);
     mapBuilder.SetDestSize(landSize, landSize);
-    mapBuilder.EnableSeamless(wrapped);
+    mapBuilder.EnableSeamless(isWrapped);
     mapBuilder.SetBounds(1.0, 5.0, 1.0, 5.0);
     mapBuilder.Build();
 
@@ -34,8 +49,10 @@ void getPerlin(const int nOctaves, const float frequency,
     utils::Image image;
     renderer.SetSourceNoiseMap(thisMap);
     renderer.SetDestImage(image);
+    renderer.Render();
 
     // no colours for now
+
     // write to file
     utils::WriterBMP writer;
     writer.SetSourceImage(image);
